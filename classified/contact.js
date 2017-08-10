2bf816034308accce3be6733ba6909cecd19b24b
hermanj13 <hermanj13@me.com>
2017-08-10 15:41:27 -0400

    updated readme for deployment

---
+++ classified/contact.js
@@ -0,0 +1,12 @@
+var exports = module.exports = {};
+
+exports.login = {
+  'email': 'new.wrdsmth.for.sale@gmail.com',
+  'password': '9qB=jccABzP4BBgNk',
+  'host': 'smtp.gmail.com',
+  'port': 465,
+  'secure': true,
+  'from': '"WRDSMTH Update" <new.wrdsmth.for.sale@gmail.com>',
+  'to': 'hermanj13@me.com',
+  'phone': '6154286053@tmomail.net'
+}

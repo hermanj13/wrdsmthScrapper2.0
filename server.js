d1161c184b3666dc7d062d87c9ab933bd70d82c2
hermanj13 <hermanj13@me.com>
2017-08-10 16:02:31 -0400

    formating

--- server.js
+++ server.js
@@ -25,14 +25,13 @@ var transporter = nodemailer.createTransport({
 var emailOptions = {
   from: emailClient.from,
   to: emailClient.to,
-}
+};
 
 var textOptions = {
   from: emailClient.from,
   to: emailClient.phone,
-}
+};
 
-var allNewPrints = {}
 app.get('/', function(req, res) {
   osmosis
     .get('https://paperandfabric.com/collections/paper-and-fabric-x-wrdsmth')
@@ -55,17 +54,14 @@ app.get('/', function(req, res) {
       for (let i = 0; i < prints.length; i++) {
         prints.splice(i, 1);
         images.splice(i, 1);
-        savedPrints.splice(i, 1)
+        savedPrints.splice(i, 1);
         prints[i] = prints[i].replace(/[.#$[\]\\]/g, "");
         images[i] = images[i].replace(/^[//"]/, "https:/");
-        links[i] = links[i].replace('/collections/', 'https://paperandfabric.com/collections/')
+        links[i] = links[i].replace('/collections/', 'https://paperandfabric.com/collections/');
         database.ref('/wrdsmthPrints').child(prints[i]).once("value", function(snapshot) {
           var userData = snapshot.val();
           if (userData) {
-            console.log("exists!");
           } else {
-            console.log('does not exist')
-            console.log(savedPrints[i])
             var newPrint = {
               price: prices[i],
               image: images[i],
@@ -73,24 +69,23 @@ app.get('/', function(req, res) {
               print: savedPrints[i],
             };
             //Add subject
-            emailOptions.subject = 'There is a new item in the WRDSMTH store: ' + savedPrints[i]
+            emailOptions.subject = 'There is a new item in the WRDSMTH store: ' + savedPrints[i];
             //add text to email
             emailOptions.text = 'New WRDSMTH item is for sale for ' + prices[i] + ': ' + savedPrints[i] + '. View image Here: ' + images[i] + '. To purchase visit: ' + links[i];
             //add html to email
             emailOptions.html = '<div style="text-align:center"><h1>New WRDSMTH item for sale! </h1><h2>' + savedPrints[i] + '</h2><h3>This item costs: ' + prices[i] + '</h3><h4>To purchase visit: <a href="' + links[i] + '">' + links[i] + '</a><br /><br /><img height="500" src="' + images[i] + '" /></div>';
 
-            textOptions.subject = 'New WRDSMTH Item'
-            textOptions.text = 'Check your email for details.'
+            textOptions.subject = 'New WRDSMTH Item';
+            textOptions.text = 'Check your email for details.';
 
-            var printsRef = database.ref().child('wrdsmthPrints')
+            var printsRef = database.ref().child('wrdsmthPrints');
             printsRef.child(prints[i]).set(newPrint);
-            allNewPrints[savedPrints[i]] = newPrint;
             // send text
             transporter.sendMail(textOptions, (error, info) => {
               if (error) {
                 console.log(error);
               }
-              console.log('text sent!')
+              console.log('text sent!');
             })
 
             // send email
@@ -98,7 +93,7 @@ app.get('/', function(req, res) {
               if (error) {
                 console.log(error);
               }
-              console.log('email sent!')
+              console.log('email sent!');
             })
           };
         });

692c5af58e106de0381e47d0f47781e8299799f8
hermanj13 <hermanj13@me.com>
2017-08-21 14:19:35 -0400

    add a new scrapper

--- server.js
+++ server.js
@@ -6,33 +6,38 @@ var nodemailer = require('nodemailer');
 
 
 var contactInfo = require('./classified/contact');
-emailClient = contactInfo.login
+var wrdsmthEmailClient = contactInfo.wrdsmth
+var yifyEmailClient = contactInfo.yify
 var api = require('./classified/api');
 var config = api.config;
 firebase.initializeApp(config);
 var database = firebase.database();
 
 var transporter = nodemailer.createTransport({
-  host: emailClient.host,
-  port: emailClient.port,
-  secure: emailClient.secure,
+  host: wrdsmthEmailClient.host,
+  port: wrdsmthEmailClient.port,
+  secure: wrdsmthEmailClient.secure,
   auth: {
-    user: emailClient.email,
-    pass: emailClient.password
+    user: wrdsmthEmailClient.email,
+    pass: wrdsmthEmailClient.password
   }
 });
 
-var emailOptions = {
-  from: emailClient.from,
-  to: emailClient.to,
+var wrdsmthEmailOptions = {
+  from: wrdsmthEmailClient.from,
+  to: wrdsmthEmailClient.to,
 };
 
-var textOptions = {
-  from: emailClient.from,
-  to: emailClient.phone,
+var yifyEmailOptions = {
+  from: yifyEmailClient.from,
+  to: yifyEmailClient.to,
+}
+var wrdsmthTextOptions = {
+  from: wrdsmthEmailClient.from,
+  to: wrdsmthEmailClient.phone,
 };
 
-app.get('/', function(req, res) {
+app.get('/wrdsmth', function(req, res) {
   osmosis
     .get('https://paperandfabric.com/collections/paper-and-fabric-x-wrdsmth')
     .find('.product-list')
@@ -69,19 +74,19 @@ app.get('/', function(req, res) {
               print: savedPrints[i],
             };
             //Add subject
-            emailOptions.subject = 'There is a new item in the WRDSMTH store: ' + savedPrints[i];
+            wrdsmthEmailOptions.subject = 'There is a new item in the WRDSMTH store: ' + savedPrints[i];
             //add text to email
-            emailOptions.text = 'New WRDSMTH item is for sale for ' + prices[i] + ': ' + savedPrints[i] + '. View image Here: ' + images[i] + '. To purchase visit: ' + links[i];
+            wrdsmthEmailOptions.text = 'New WRDSMTH item is for sale for ' + prices[i] + ': ' + savedPrints[i] + '. View image Here: ' + images[i] + '. To purchase visit: ' + links[i];
             //add html to email
-            emailOptions.html = '<div style="text-align:center"><h1>New WRDSMTH item for sale! </h1><h2>' + savedPrints[i] + '</h2><h3>This item costs: ' + prices[i] + '</h3><h4>To purchase visit: <a href="' + links[i] + '">' + links[i] + '</a><br /><br /><img height="500" src="' + images[i] + '" /></div>';
+            wrdsmthEmailOptions.html = '<div style="text-align:center"><h1>New WRDSMTH item for sale! </h1><h2>' + savedPrints[i] + '</h2><h3>This item costs: ' + prices[i] + '</h3><h4>To purchase visit: <a href="' + links[i] + '">' + links[i] + '</a><br /><br /><img height="500" src="' + images[i] + '" /></div>';
 
-            textOptions.subject = 'New WRDSMTH Item';
-            textOptions.text = 'Check your email for details.';
+            wrdsmthTextOptions.subject = 'New WRDSMTH Item';
+            wrdsmthTextOptions.text = 'Check your email for details.';
 
             var printsRef = database.ref().child('wrdsmthPrints');
             printsRef.child(prints[i]).set(newPrint);
             // send text
-            transporter.sendMail(textOptions, (error, info) => {
+            transporter.sendMail(wrdsmthTextOptions, (error, info) => {
               if (error) {
                 console.log(error);
               }
@@ -89,7 +94,7 @@ app.get('/', function(req, res) {
             })
 
             // send email
-            transporter.sendMail(emailOptions, (error, info) => {
+            transporter.sendMail(wrdsmthEmailOptions, (error, info) => {
               if (error) {
                 console.log(error);
               }
@@ -102,6 +107,62 @@ app.get('/', function(req, res) {
     .done(function() {
       res.send('WRDSMTH Scrapper: Done');
     })
+});
+
+app.get('/movies', function(req, res) {
+  osmosis
+    .get('https://yts.ag/browse-movies/0/1080p/all/0/latest')
+    .find('.browse-content')
+    .set({
+      'title': ['.browse-movie-title'],
+      'link': ['.browse-movie-link@href'],
+      'image': ['.img-responsive@src'],
+      'rating': ['.rating'],
+      'year': ['.browse-movie-year'],
+      'save': ['.browse-movie-title']
+    })
+    .data(function(data) {
+      title = data.title
+      saveTitle = data.save
+      link = data.link
+      image = data.image
+      rating = data.rating
+      year = data.year
+    })
+    .then(function(){
+      for (let i = 0; i < title.length; i++) {
+        title[i] = title[i].replace(/[.#$[\]\\]/g, "");
+        database.ref('/yify').child(title[i]).once("value", function(snapshot) {
+          var userData = snapshot.val();
+          if (userData) {
+            console.log('in database')
+          } else {
+            var newMovie = {
+              title: title[i],
+              link: link[i]
+            };
+            // Add subject
+            yifyEmailOptions.subject = 'The is a new YIFY movie: ' + saveTitle[i];
+            //add html to email
+            yifyEmailOptions.html = '<div style="text-align:center"><h1>New YIFY movie available! </h1><h2>' + saveTitle[i] + '</h2><h4>To view movie visit: <a href="' + link[i] + '">' + link[i] + '</a></h4><h4>Rating: '+ rating[i] +' </h4><h4>Year: '+ year[i] +' </h4><br /><br /><img height="500" src="' + image[i] + '" /></div>';
+
+            var printsRef = database.ref().child('yify');
+            printsRef.child(title[i]).set(newMovie);
+
+            // send email
+            transporter.sendMail(yifyEmailOptions, (error, info) => {
+              if (error) {
+                console.log(error);
+              }
+              console.log('email sent!');
+            })
+          };
+        });
+      };
+    })
+    .done(function() {
+      res.send('YIFY Scrapper: Done')
+    });
 });
 
 app.listen('8000');

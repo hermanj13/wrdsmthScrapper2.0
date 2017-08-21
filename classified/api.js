3d6db0e9cb6a58590548e1fa1af4759206e49d13
hermanj13 <hermanj13@me.com>
2017-08-21 14:22:14 -0400

    push

--- classified/api.js
+++
@@ -1,9 +0,0 @@
-var exports = module.exports = {};
-exports.config = {
- apiKey: "AIzaSyBxSbQODDnlPYtVUUKKJfob9C9S1EeCfyQ",
- authDomain: "wrdsmth-1ec50.firebaseapp.com",
- databaseURL: "https://wrdsmth-1ec50.firebaseio.com",
- projectId: "wrdsmth-1ec50",
- storageBucket: "wrdsmth-1ec50.appspot.com",
- messagingSenderId: "174497731489"
-};

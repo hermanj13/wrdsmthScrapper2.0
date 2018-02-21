dd1bbef845521f34e09068e7593eacb8bf385861
hermanj13 <hermanj13@me.com>
2018-02-20 22:01:00 -0500

    redesign

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,36 +1,66 @@
-var osmosis = require('osmosis');
+const osmosis = require('osmosis')
+var request = require('request-promise')
 
-var api = require('../classified/api');
-var config = api.config;
+const api = require('../classified/api')
+const config = api.config
 
-var firebase = require('firebase');
-firebase.initializeApp(config);
-var database = firebase.database();
+const firebase = require('firebase')
+firebase.initializeApp(config)
+const database = firebase.database()
 
-var nodemailer = require('nodemailer');
-var contactInfo = require('../classified/contact');
-var wrdsmthEmailClient = contactInfo.wrdsmth;
+const twilloInfo = require('../classified/contact').twillo
 
-var transporter = nodemailer.createTransport({
-  host: wrdsmthEmailClient.host,
-  port: wrdsmthEmailClient.port,
-  secure: wrdsmthEmailClient.secure,
-  auth: {
-    user: wrdsmthEmailClient.email,
-    pass: wrdsmthEmailClient.password
-  }
-});
+const accountSid = twilloInfo.accountSid
+const authToken = twilloInfo.authToken
+const client = require('twilio')(accountSid, authToken)
+// const MessagingResponse = require('twilio').twiml.MessagingResponse
 
-module.exports = function(app) {
-  app.get('/wrdsmth', function(req, res) {
-    var wrdsmthEmailOptions = {
-      from: wrdsmthEmailClient.from,
-      to: wrdsmthEmailClient.to,
-    };
-    var wrdsmthTextOptions = {
-      from: wrdsmthEmailClient.from,
-      to: wrdsmthEmailClient.phone,
-    };
+module.exports = function (app) {
+  app.post('/recieve', function (req, res) {
+    if (req.body.From === '+16154286053') {
+      var search = req.body.Body.split(' ')
+      // TODO: add inaditional switch cases for information about the movies on request
+      switch (search[0]) {
+        case 'Add':
+        case 'add':
+          search.splice(0, 1)
+          search = search.join().split(',')
+          search.forEach(function (value) {
+            database.ref('/yify').orderByChild('id').equalTo(parseInt(value)).on('value', function (snapshot) {
+              var searchTerm = Object.keys(snapshot.val())[0]
+              database.ref('/download').child(searchTerm).once('value', function (data) {
+                if (data.val()) {} else {
+                  var downloadRef = database.ref().child('download')
+                  downloadRef.child(searchTerm).set(snapshot.val()[searchTerm])
+                }
+              })
+            })
+          })
+          break
+        case 'remove':
+        case 'Remove':
+          search.splice(0, 1)
+          search = search.join().split(',')
+          search.forEach(function (value) {
+            database.ref('/yify').orderByChild('id').equalTo(parseInt(value)).on('value', function (snapshot) {
+              var searchTerm = Object.keys(snapshot.val())[0]
+              database.ref('/download').child(searchTerm).once('value', function (data) {
+                if (data.val()) {
+                  database.ref('/download').child(searchTerm).remove()
+                }
+              })
+            })
+          })
+          break
+      }
+      // TODO: add in response functionality
+      res.writeHead(200, {'Content-Type': 'text/xml'})
+      res.send()
+    }
+  })
+
+  app.get('/wrdsmth', function (req, res) {
+    var prints, links, savedPrints, prices, images
     osmosis
       .get('https://paperandfabric.com/collections/paper-and-fabric-x-wrdsmth')
       .find('.product-list')
@@ -41,83 +71,59 @@ module.exports = function(app) {
         'link': ['.title @href'],
         'saved': ['.title']
       })
-      .data(function(data) {
-        links = data.link;
-        prints = data.title;
-        savedPrints = data.saved;
-        prices = data.prices;
-        images = data.image;
+      .data(function (data) {
+        links = data.link
+        prints = data.title
+        savedPrints = data.saved
+        prices = data.prices
+        images = data.image
       })
-      .then(function() {
+      .then(function () {
         for (let i = 0; i < prints.length; i++) {
-          prints.splice(i, 1);
-          images.splice(i, 1);
-          savedPrints.splice(i, 1);
-          prints[i] = prints[i].replace(/[.#$[\]\\]/g, "");
-          images[i] = images[i].replace(/^[//"]/, "https:/");
-          links[i] = links[i].replace('/collections/', 'https://paperandfabric.com/collections/');
-          database.ref('/wrdsmthPrints').child(prints[i]).once("value", function(snapshot) {
-            var userData = snapshot.val();
+          prints.splice(i, 1)
+          images.splice(i, 1)
+          savedPrints.splice(i, 1)
+          prints[i] = prints[i].replace(/[.#$[\]\\]/g, '')
+          images[i] = images[i].replace(/^[//"]/, 'https:/')
+          links[i] = links[i].replace('/collections/', 'https://paperandfabric.com/collections/')
+          database.ref('/wrdsmthPrints').child(prints[i]).once('value', function (snapshot) {
+            var userData = snapshot.val()
             if (userData) {} else {
               var newPrint = {
                 price: prices[i],
                 image: images[i],
                 link: links[i],
                 print: savedPrints[i],
-              };
-              //Add subject
-              wrdsmthEmailOptions.subject = 'There is a new item in the WRDSMTH store: ' + savedPrints[i];
-              //add text to email
-              wrdsmthEmailOptions.text = 'New WRDSMTH item is for sale for ' + prices[i] + ': ' + savedPrints[i] + '. View image Here: ' + images[i] + '. To purchase visit: ' + links[i];
-              //add html to email
-              wrdsmthEmailOptions.html = '<div style="text-align:center"><h1>New WRDSMTH item for sale! </h1><h2>' + savedPrints[i] + '</h2><h3>This item costs: ' + prices[i] + '</h3><h4>To purchase visit: <a href="' + links[i] + '">' + links[i] + '</a><br /><br /><img height="500" src="' + images[i] + '" /></div>';
-
-              wrdsmthTextOptions.subject = 'New WRDSMTH Item';
-              wrdsmthTextOptions.text = 'Print Name: ' + savedPrints[i] + "\nPrice: " +
-                prices[i] + '\nLink: ' + links[i];
-              wrdsmthTextOptions.attachments = [{
-                filename: 'image.png',
-                path: images[i],
-                cid: wrdsmthTextOptions.from
-              }]
-              var printsRef = database.ref().child('wrdsmthPrints');
-              printsRef.child(prints[i]).set(newPrint);
+                timestamp: firebase.database.ServerValue.TIMESTAMP
+              }
+              var printsRef = database.ref().child('wrdsmthPrints')
+              printsRef.child(prints[i]).set(newPrint)
               // send text
-              transporter.sendMail(wrdsmthTextOptions, (error, info) => {
-                if (error) {
-                  console.log(error);
-                } else{
-                  console.log('text sent!');
-                }
-              })
-
-              // send email
-              transporter.sendMail(wrdsmthEmailOptions, (error, info) => {
-                if (error) {
-                  console.log(error);
-                } else{
-                  console.log('email sent!');
-                }
+              twilloInfo.wrdsmth.forEach(function (value) {
+                client.messages
+                  .create({
+                    body: `Print Name: ${savedPrints[i]} \nPrice: ${prices[i]} \nLink: ${links[i]}`,
+                    to: value,
+                    from: twilloInfo.from,
+                    mediaUrl: images[i]
+                  })
+                  .then(message => process.stdout.write(message.sid))
               })
             };
-          });
+          })
         };
       })
-      .done(function() {
-        res.send('WRDSMTH Scrapper: Done');
+      .done(function () {
+        res.send('WRDSMTH Scrapper: Done')
       })
-  });
+  })
 
-  app.get('/movies', function(req, res) {
-    var yifyEmailClient = contactInfo.yify;
-    var yifyEmailOptions = {
-      from: yifyEmailClient.from,
-      to: yifyEmailClient.to,
-    };
-    var yifyTextOptions = {
-      from: yifyEmailClient.from,
-      to: yifyEmailClient.phone
-    };
+  app.get('/movies', function (req, res) {
+    var title, saveTitle, link, image, rating, year, lastObject, keys
+    database.ref('/yify').once('value', function (snapshot) {
+      keys = Object.keys(snapshot.val() || {})
+      lastObject = keys.length
+    })
     osmosis
       .get('https://yts.ag/browse-movies/0/1080p/all/0/latest')
       .find('.browse-content')
@@ -129,7 +135,7 @@ module.exports = function(app) {
         'year': ['.browse-movie-year'],
         'save': ['.browse-movie-title']
       })
-      .data(function(data) {
+      .data(function (data) {
         title = data.title
         saveTitle = data.save
         link = data.link
@@ -137,55 +143,46 @@ module.exports = function(app) {
         rating = data.rating
         year = data.year
       })
-      .then(function() {
+      .then(function () {
         for (let i = 0; i < title.length; i++) {
-          title[i] = title[i].replace(/[.#$[\]\\]/g, "");
-          database.ref('/yify').child(title[i]).once("value", function(snapshot) {
-            var userData = snapshot.val();
+          title[i] = title[i].replace(/[.#$[\]\\]/g, '')
+          database.ref('/yify').child(title[i]).once('value', function (snapshot) {
+            var userData = snapshot.val()
             if (userData) {} else {
-              var newMovie = {
-                title: title[i],
-                link: link[i]
-              };
-              // Add subject
-              yifyEmailOptions.subject = 'The is a new YIFY movie: ' + saveTitle[i];
-              //add html to email
-              yifyEmailOptions.html = '<div style="text-align:center"><h1>New YIFY movie available! </h1><h2>' + saveTitle[i] + '</h2><h4>To view movie visit: <a href="' + link[i] + '">' + link[i] + '</a></h4><h4>Rating: ' + rating[i] + ' </h4><h4>Year: ' + year[i] + ' </h4><br /><br /><img height="500" src="' + image[i] + '" /></div>';
-
-              var printsRef = database.ref().child('yify');
-              printsRef.child(title[i]).set(newMovie);
-
-              // send email
-              transporter.sendMail(yifyEmailOptions, (error, info) => {
-                if (error) {
-                  console.log(error);
-                }else{
-                  console.log('email sent!');
+              var search = title[i].replace(/\s/g, '+')
+              var newBody
+              request(`http://www.omdbapi.com/?apikey=trilogy&t=${search}&plot=full`, function (error, response, body) {
+                if (error) { console.log(error) }
+                newBody = JSON.parse(body)
+              }).then(function () {
+                var newMovie = {
+                  id: lastObject += 1,
+                  title: title[i],
+                  link: link[i],
+                  image: image[i],
+                  timestamp: firebase.database.ServerValue.TIMESTAMP,
+                  plot: newBody.Plot || null,
+                  genre: newBody.Genre || null,
+                  actors: newBody.Actors || null
                 }
-              })
+                var movieRef = database.ref().child('yify')
+                movieRef.child(title[i]).set(newMovie)
 
-              yifyTextOptions.subject = 'New YIFY Movie';
-              yifyTextOptions.text = 'Movie name: ' + saveTitle[i] + "\nYear: " +
-                year[i] + '\nRating: ' + rating[i];
-              yifyTextOptions.attachments = [{
-                filename: 'image.png',
-                path: image[i],
-                cid: yifyTextOptions.from
-              }]
-              // send text
-              transporter.sendMail(yifyTextOptions, (error, info) => {
-                if (error) {
-                  console.log(error);
-                }else{
-                  console.log('text sent!');
-                }
+                // send text
+                client.messages
+                  .create({
+                    body: `Movie name: ${saveTitle[i]} \nPlot: ${newBody.Plot || 'n/a'} \nRating: ${rating[i]} \nGenre: ${newBody.Genre || 'n/a'} \nActors: ${newBody.Actors || 'n/a'} \nYear: ${year[i]} \nText 'Add ${lastObject}' to save for download`,
+                    to: twilloInfo.yify,
+                    from: twilloInfo.from
+                  })
+                  .then(message => process.stdout.write(message.sid))
               })
-            };
-          });
+            }
+          })
         };
       })
-      .done(function() {
+      .done(function () {
         res.send('YIFY Scrapper: Done')
-      });
-  });
-};
+      })
+  })
+}
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var app = express();
-require("./routes/apiRoutes")(app);
-app.listen('8000');
-exports = module.exports = app;
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+require('./routes/apiRoutes')(app)
+app.listen('8000')
+exports = module.exports = app
